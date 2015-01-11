debac8eccff50d747cb2ddceeef74c8c68763c81
MasterDecompiler <shadinachat.sn@gmail.com>
2015-01-11 15:21:52 -0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,33 @@
+print("Welcome To Simple Dox!")
+print("Made By Syriamanal")
+#----- About -----
+FullName = input("Enter Full Name: ")
+Address = input("Enter Home Address: ")
+City = input("Enter City: ")
+Zipcode = input("Enter Zipcode: ")
+State = input("Enter State: ")
+Country = input("Enter Country: ")
+
+#----- E-Mails -----
+Email = input("Enter Emails: ")
+
+#----- ISP Information -----
+IP = input("Enter IP Address: ")
+Location = input("Enter Location: ")
+Latitude = input("Enter Latitude & Longitude:")
+ISP = input("Enter ISP: ")
+Domain = input("Enter Domain: ")
+NetSpeed = input("Enter Net Speed: ")
+IDD = input("Enter IDD: ")
+
+#-----Print -----
+print("----- About -----")
+print (FullName + ", " + Address + ", " + City + ", " + Zipcode + ", " + State + ", " + ", " + Country)
+
+#----- E-Mails -----
+print("----- E-Mails -----")
+print(Email + " ")
+
+#----- ISP Information -----
+print("----- ISP Information -----")
+print(IP + ", " + Location + ", " + Latitude + ", " + ISP + ", " + Domain + ", " + NetSpeed + ", " + IDD) 
